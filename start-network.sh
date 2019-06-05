c46890fff071d0928a5955b9029795812380781d
Balazs Prehoda <prehoda.balazs@gmail.com>
2019-06-05 09:51:48 +0200

    Add generate script to start script.

--- start-network.sh
+++ start-network.sh
@@ -6,6 +6,8 @@ export PATH=$PATH:$GOBIN
 export FABRIC_CFG_PATH=$PWD
 export CHANNEL_NAME=mychannel
 
+source ./generate-artifacts.sh
+
 COMPOSE_PROJECT_NAME=$CHANNEL_NAME docker-compose -f docker-compose-cli.yaml up -d
 
 docker exec -it cli bash
